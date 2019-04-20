6ba4ca7844563110fd55ad2d38f492f329d4f30d
PS <PS@IYAL-PS>
2019-04-20 11:32:26 +0530

    Hibernate session factory and Context log messages updated

--- RSV/src/sathish/app/util/HibernateSessionFactoryListener.java
+++ RSV/src/sathish/app/util/HibernateSessionFactoryListener.java
@@ -5,12 +5,9 @@ package sathish.app.util;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.*;
-import org.hibernate.service.ServiceRegistry;
 import org.hibernate.*;
 /**
  * @author root
@@ -22,11 +19,8 @@ public class HibernateSessionFactoryListener implements ServletContextListener {
 
 	@Override
 	public void contextInitialized(ServletContextEvent servletContextEvent) {
-		// TODO Auto-generated method stub
-		logger.debug("Initialize Context");
+		logger.debug("...Initialize Context...");
 		SessionFactory sessionFactory;
-		ServiceRegistry serviceRegistry;
-
 		Configuration configuration = new Configuration();
 		configuration.configure();
 
@@ -35,23 +29,22 @@ public class HibernateSessionFactoryListener implements ServletContextListener {
 
 		sessionFactory = new Configuration().configure().buildSessionFactory();
 		
-		logger.debug("SessionFactory created successfully");
+		logger.debug("...SessionFactory created successfully...");
 
 		servletContextEvent.getServletContext().setAttribute("SessionFactory", sessionFactory);
 
-		logger.debug("Hibernate SessionFactory Configured successfully");
+		logger.debug("...Hibernate SessionFactory Configured successfully...");
 	}
 
 	@Override
 	public void contextDestroyed(ServletContextEvent servletContextEvent) {
-		// TODO Auto-generated method stub
-		logger.debug("Destroy Context");
+		logger.debug("...Destroy Context...");
 		SessionFactory sessionFactory = (SessionFactory) servletContextEvent.getServletContext().getAttribute("SessionFactory");
 		if (sessionFactory != null && !sessionFactory.isClosed()) {
-			logger.debug("Closing sessionFactory");
+			logger.debug("...Closing sessionFactory...");
 			sessionFactory.close();
 		}
-		logger.debug("Released Hibernate sessionFactory resource");
+		logger.debug("...Released Hibernate sessionFactory resource...");
 	}
 
 }
